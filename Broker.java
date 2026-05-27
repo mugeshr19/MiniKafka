4e6bd6a98c35531ccc2bc965dc5524bc5a7011ae
mugeshr19 <mugesh.r1906@gmail.com>
2026-05-27 20:07:08 +0530

    added array of object for the consumer which store the which consumer subscribed to which topic so that the consumer will receive the event of that particular topic , for now we are creating new thread for each consumer and producer , the producer logic is same as the previous but slight change we have added a for loop to send event to consumer and a function for consumer to subscribe a topic and class of the consumerhandler

--- Broker.java
+++ Broker.java
@@ -1,5 +1,6 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -8,6 +9,8 @@ import java.util.HashMap;
 
 public class Broker{
     static HashMap<String,ArrayList<String>> topics = new HashMap<>();
+
+    static ArrayList<ConsumerHandler> consumers = new ArrayList<>();
     public static void main(String[] args){
         try{
             ServerSocket serverSocket = new ServerSocket(9092);
@@ -16,8 +19,20 @@ public class Broker{
             while(true){
 
                 Socket socket = serverSocket.accept();
-                System.out.println("Producer connected");
+                System.out.println("Client connected");
+                
+                new Thread(()->{
+                    handleClient(socket);
+                }).start();
+                
+            }
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
+    public static void handleClient(Socket socket){
+            try{
                 BufferedReader reader = new BufferedReader(
                     new InputStreamReader(socket.getInputStream())
                 );
@@ -27,7 +42,7 @@ public class Broker{
 
                     System.out.println("Received Message: " + data);
 
-                    if(data.contains("\"topic\"")&&data.contains("\"message\"")){
+                    if(data.contains("\"type\":\"produce\"")){
 
                         String topic = extractValue(data,"topic");
 
@@ -40,20 +55,35 @@ public class Broker{
                         );
                         }
                         topics.get(topic).add(message);
+
+                        System.out.println("stored in topic" + topic);
+                        System.out.println("\nCurrent topic : ");
+                        System.out.println(topics);
+
+                        for(ConsumerHandler consumer : consumers){
+                            if(consumer.topic.equals(topic)){
+                                consumer.writer.println("{\"topic\":\""+topic+"\","+"\"message\":\""+message+"\"}");
+                            }
+                        }
+
+                    }
+                    else if(data.contains("\"type\":\"consume\"")){
+                        String topic = extractValue(data, "topic");
+                        PrintWriter writer = new PrintWriter(
+                            socket.getOutputStream(),
+                            true
+                        );
+                        consumers.add(new ConsumerHandler(topic,writer));
                     }
 
-                    System.out.println("\nCurrent topic : ");
-                    System.out.println(topics);
                 }
-
-                System.out.println("Producer disconnected");
+                System.out.println("Client disconnected");
 
                 socket.close();
-            }
-
-        }catch (Exception e) {
-            e.printStackTrace();
-        }
+                
+            }catch(Exception e){
+                    e.printStackTrace();
+                }
     }
     public static String extractValue(String json,String key){
         String search = "\"" + key + "\":\"";
@@ -67,5 +97,13 @@ public class Broker{
         int end = json.indexOf("\"", start);
 
         return json.substring(start, end);
+    }
+    static class ConsumerHandler{
+        String topic;
+        PrintWriter writer;
+        ConsumerHandler(String topic,PrintWriter writer){
+            this.topic = topic;
+            this.writer = writer;
+        }
     }
 }
